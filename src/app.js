457543a145008b282386830205487c9d3cefc90d
Sumit Dey <sumitdey851@outlook.com>
2020-09-03 14:24:39 +0530

    Added documentation

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
+//importing npm(s)
 const path = require('path')
 const express = require('express')
-const hbs = require('hbs')
+const hbs = require('hbs')  //Handlebars is a simple templating language.
 
+//importing own modules
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 //Create an Express application
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000   //using environment variable PORT to tell heroku what port to listen on
 
 
 //Define paths for Express config
@@ -15,7 +17,7 @@ const publicDirPath = path.join(__dirname, '../public')
 const viewPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
-//Setup handlebars engine and views location
+//Setup handlebars engine and views/partials location
 app.set('view engine', 'hbs')
 app.set('views', viewPath)
 hbs.registerPartials(partialsPath)
@@ -23,21 +25,21 @@ hbs.registerPartials(partialsPath)
 //Setup static directory to serve
 app.use(express.static(publicDirPath))
 
-app.get('', (req, res) => {
+app.get('', (req, res) => {     //renders the index.hbs view
     res.render('index', {
         title: 'Weather',
         name: 'Sumit Dey'
     })
 })
 
-app.get('/about', (req, res) => {
+app.get('/about', (req, res) => {   //renders about.hbs view
     res.render('about', {
         title: 'About',
         name: 'Sumit Dey'
     })
 })
 
-app.get('/help', (req, res) => {
+app.get('/help', (req, res) => {    //renders help.hbs view
     res.render('help', {
         helpText: 'Version 1.8',
         title: 'Help',
@@ -78,7 +80,7 @@ app.get('/help/*', (req, res) => {  //for urls that contain anything typed after
     })
 })
 
-app.get('*', (req, res) => {
+app.get('*', (req, res) => {    //for all urls except those defined in this file
     res.render('404', {
         title: 'Error 404!',
         errorMessage: 'Page not found!',
@@ -86,6 +88,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(port, () => {
+app.listen(port, () => {    //Binds and listens for connections on the specified host and port.
     console.log('Server is up on port ' + port)
 })
